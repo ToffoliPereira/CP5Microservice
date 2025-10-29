91d8def88785d33ed55dd86ab7ab2270feb6399f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-29 19:54:30 -0300

    Etapa 1 - Modelagem do domínio

---
+++ Domain/MovimentacaoEstoque.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class MovimentacaoEstoque
+    {
+        public string Tipo { get; set; }
+        public string Qtd { get; set; }
+        public DateTime DataMovimentacao { get; set; }
+        public string Lote { get; set; }
+        public DateTime DataValidade { get; set; }
+    }
+}
---
+++ Domain/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public string PrecoUnitario { get; set; }
+        public string Qtdmin { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+    }
+}
